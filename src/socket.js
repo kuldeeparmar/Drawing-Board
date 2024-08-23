e3dd8e1a487fe470663559f9d6a25136bdfa9c30
Kuldeep Parmar <kuldeepparmar246@gmail.com>
2024-08-24 01:41:34 +0530

    Server Rendering

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+const URL = process.env.NODE_ENV === 'production' ? 'https://drawing-board-server-6jrp.onrender.com' : 'http://localhost:5000';
+
+export const socket = io(URL);
